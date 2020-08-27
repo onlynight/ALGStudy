6613ce034af9e94162a2195eb9348acf9ff92ef9
only.night@qq.com <only.night@qq.com>
2020-08-27 18:59:02 +0800

    add KMP

---
+++ src/com/github/onlynight/pattern/KMPPattern.java
@@ -0,0 +1,12 @@
+package com.github.onlynight.pattern;
+
+/**
+ * <b>KMP模式匹配算法</b>
+ * https://blog.csdn.net/l577217/article/details/80373822
+ */
+public class KMPPattern implements Pattern {
+    @Override
+    public int match(String targetStr, String patternStr) {
+        return 0;
+    }
+}
