94f10d1fdb1c6bea8d1750cfc61adf3ce9efa584
only.night@qq.com <only.night@qq.com>
2020-08-27 19:18:59 +0800

    添加常用模式匹配算法链接

--- src/com/github/onlynight/pattern/Pattern.java
+++ src/com/github/onlynight/pattern/Pattern.java
@@ -2,6 +2,9 @@ package com.github.onlynight.pattern;
 
 /**
  * 字符串模式匹配
+ * 常用模式匹配算法
+ * https://www.cnblogs.com/iwangzhengchao/p/10283326.html
+ * https://www.jianshu.com/p/8690bd38ea62
  */
 public interface Pattern {
 
