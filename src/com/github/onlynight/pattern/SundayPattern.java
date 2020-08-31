428cf4b7312ec95996662ed19653519edfe0046e
only.night@qq.com <only.night@qq.com>
2020-08-31 19:39:19 +0800

    add sunday pattern

--- src/com/github/onlynight/pattern/SundayPattern.java
+++ src/com/github/onlynight/pattern/SundayPattern.java
@@ -4,7 +4,17 @@ public class SundayPattern implements Pattern {
 
     @Override
     public int match(String targetStr, String patternStr) {
-        return 0;
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        String target = "qwerrtyuyuiopqwerqwer";
+        String pattern = "i";
+        int result = new SundayPattern().match(target, pattern);
+        System.out.println(result);
+        pattern = "er";
+        result = new SundayPattern().match(target, pattern);
+        System.out.println(result);
     }
     
 }
